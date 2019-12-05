46b4c938ee0ed7c2291d9b29b68c952e94e4a87c
manishpal022 <manishpal.055@gmail.com>
2019-12-05 15:08:41 +0530

    rename 'number' to 'quantity' rename flavours, size, status choices entries minimise the name field max_length

--- order_app/models.py
+++ order_app/models.py
@@ -1,37 +1,38 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 FLAVOURS_CHOICE = (
-    ('MARGARITA', 'Margarita'),
-    ('MARINARA', 'Marinara'),
-    ('SALAMI', 'Salami'),
+    ('Margarita', 'Margarita'),
+    ('Marinara', 'Marinara'),
+    ('Salami', 'Salami'),
 )
 
 SIZE_CHOICE = (
-    ('SMALL', 'Small'),
-    ('MEDIUM', 'Medium'),
-    ('LARGE', 'Large'),
+    ('Small', 'Small'),
+    ('Medium', 'Medium'),
+    ('Large', 'Large'),
 )
 
 STATUS_CHOICE = (
-    ('OPEN', 'Open'),
-    ('ACCEPTED', 'Accepted'),
-    ('PREPARING', 'Preparing'),
-    ('OUT FOR DELIVERY', 'Out for Delivery'),
+    ('Open', 'Open'),
+    ('Accepted', 'Accepted'),
+    ('Preparing', 'Preparing'),
+    ('OnWay', 'OnWay'),
     ('Delivered', 'Delivered'),
 )
 
 
 class PizzaOrder(models.Model):
-    name = models.CharField(max_length=120, blank=False)
+    name = models.CharField(max_length=50, blank=False)
     flavours = models.CharField(max_length=20, choices=FLAVOURS_CHOICE)
-    number = models.IntegerField(default=1)
-    size = models.CharField(max_length=10, choices=SIZE_CHOICE, default='MEDIUM')
-    customer_name = models.CharField(max_length=120, blank=False)
+    quantity = models.IntegerField()
+    size = models.CharField(max_length=10, choices=SIZE_CHOICE)
+    customer_name = models.CharField(max_length=30, blank=False)
     customer_address = models.TextField(blank=False)
     ordered_time = models.DateTimeField(default=timezone.now, editable=False)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='OPEN')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='Open')
 
     def __str__(self):
         return self.name
--- order_app/serializers.py
+++ order_app/serializers.py
@@ -1,19 +1,25 @@
-from rest_framework import routers, serializers, viewsets
+from rest_framework import serializers
 from .models import PizzaOrder
+from rest_framework.validators import UniqueTogetherValidator
 
 
 class PizzaOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = PizzaOrder
-        fields = ('id', 'name', 'flavours', 'number', 'size', 'customer_name', 'customer_address', 'ordered_time', 'status',)
-        # excludes = ('status',)
-        # exclude = ('status',)
-        # read_only_fields = ('status', 'ordered_time',)
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=PizzaOrder.objects.all(),
+                fields=['name', 'flavours', 'size', 'customer_name', 'customer_address'],
+                message='This field should be unique'
+            )
+        ]
+        fields = ('id', 'name', 'flavours', 'quantity', 'size', 'customer_name', 'customer_address', 'ordered_time', 'status')
         extra_kwargs = {
             'id': {'read_only': True},
             'name': {'read_only': False},
             'flavours': {'read_only': False},
-            'number': {'read_only': False},
+            'quantity': {'read_only': False},
             'size': {'read_only': False},
             'customer_name': {'read_only': False},
             'customer_address': {'read_only': False},
@@ -25,12 +31,12 @@ class PizzaOrderSerializer(serializers.ModelSerializer):
 class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = PizzaOrder
-        fields = ('id', 'name', 'flavours', 'number', 'size', 'customer_name', 'customer_address', 'ordered_time', 'status',)
+        fields = ('id', 'name', 'flavours', 'quantity', 'size', 'customer_name', 'customer_address', 'ordered_time', 'status',)
         extra_kwargs = {
             'id': {'read_only': True},
             'name': {'read_only': True},
             'flavours': {'read_only': True},
-            'number': {'read_only': True},
+            'quantity': {'read_only': True},
             'size': {'read_only': True},
             'customer_name': {'read_only': True},
             'customer_address': {'read_only': True},
