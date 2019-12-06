ef100fd856042d5ef0d438466771cfcf5d2e2aab
manishpal022 <manishpal.055@gmail.com>
2019-12-06 14:26:56 +0530

    add test cases

--- order_app/tests.py
+++ order_app/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.urls import include, path, reverse
+from rest_framework.test import APITestCase, URLPatternsTestCase
+from rest_framework import routers
+from order_app import views
+from .models import PizzaOrder
+from .serializers import PizzaOrderSerializer
 
-# Create your tests here.
+router = routers.DefaultRouter()
+router.register(r'customer_orders', views.CustomerViewSet, base_name='pizzaorder')
+
+
+class ListAllTests(APITestCase, URLPatternsTestCase):
+
+    urlpatterns = [
+        path('', include(router.urls)),
+    ]
+    url = '/customer_orders/'
+
+    def test_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(200, response.status_code)
